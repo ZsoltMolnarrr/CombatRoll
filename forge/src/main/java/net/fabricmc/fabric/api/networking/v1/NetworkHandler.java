f2176dc7962e50fe93aa277f668db8b851134b3b
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2023-10-31 23:20:41 +0100

    Update Forge network stack

--- forge/src/main/java/net/fabricmc/fabric/api/networking/v1/NetworkHandler.java
+++ forge/src/main/java/net/fabricmc/fabric/api/networking/v1/NetworkHandler.java
@@ -2,15 +2,23 @@ package net.fabricmc.fabric.api.networking.v1;
 
 import net.combatroll.CombatRoll;
 import net.minecraft.util.Identifier;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.simple.SimpleChannel;
+import net.minecraftforge.network.ChannelBuilder;
+import net.minecraftforge.network.SimpleChannel;
 
 public class NetworkHandler {
     public static SimpleChannel INSTANCE;
     private static int ID = 0;
 
     public static void registerMessages(){
-        INSTANCE = NetworkRegistry.newSimpleChannel(new Identifier(CombatRoll.MOD_ID, "network"), () -> "1.0", s -> true, s -> true);
-        INSTANCE.registerMessage(ID++, PacketWrapper.class, PacketWrapper::encode, PacketWrapper::decode, PacketWrapper::handle);
+        INSTANCE = ChannelBuilder.named(new Identifier(CombatRoll.MOD_ID, "network"))
+                .networkProtocolVersion(1)
+                .acceptedVersions((s, v) -> true)
+                .simpleChannel();
+        INSTANCE.messageBuilder(PacketWrapper.class)
+                .encoder(PacketWrapper::encode)
+                .decoder(PacketWrapper::decode)
+                .consumerNetworkThread(PacketWrapper::handle)
+                .add();
+
     }
 }
